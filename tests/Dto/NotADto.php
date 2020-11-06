0e5949c08a202b117b9be254ba59c52110349015
Matthias Burtscher <matthias.burtscher@fusonic.net>
2020-11-06 13:26:58 +0100

    Added license information for all open source libraries (closes #14)
    This change introduces the MIT license for all open source libraries and includes the following changes:
    - SPDX identifiers added to composer.json files
    - LICENSE file added to each repository
    - Short licensing header added to all source files
    - Copyright configuration for PhpStorm

--- tests/Dto/NotADto.php
+++ tests/Dto/NotADto.php
@@ -1,5 +1,8 @@
 <?php
 
+// Copyright (c) Fusonic GmbH. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for license information.
+
 namespace Fusonic\HttpKernelExtensions\Tests\Dto;
 
 class NotADto
